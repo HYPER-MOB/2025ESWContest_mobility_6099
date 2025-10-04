7f91aefa667ddc1bb2cf982a8b8bcb0eff723174
INT\reiy0902 <reiy0902@naver.com>
2025-10-04 12:48:53 +0900

    [Library-CAN] feat : CORE 코드 수정
    subscribe / registerJob 부분에서 에러 코드와 job/subID가 겹치는 현상을 발견, id는 포인터로 전달하도록 변경

--- adapterfactory.c
+++ adapterfactory.c
@@ -1,14 +1,12 @@
 #include "adapter.h"
 
-Adapter* adapter_debug_new(void);
 Adapter* adapter_linux_new(void);
 Adapter* adapter_esp32_new(void);
 
 Adapter* create_adapter(can_device_t device) {
     switch (device) {
-        //case CAN_DEVICE_LINUX:  return adapter_linux_new();
+        case CAN_DEVICE_LINUX:  return adapter_linux_new();
         case CAN_DEVICE_ESP32:  return adapter_esp32_new();
-        //case CAN_DEVICE_DEBUG:  return adapter_debug_new();
         default:                return NULL;           
     }
 }
