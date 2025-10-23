908638ff8d9f8bb2bca374be82510260c3f91678
INT\reiy0902 <reiy0902@naver.com>
2025-10-23 21:48:45 +0900

    [POW-Seat] fix : 미사용 코드 제거
    미사용 코드 제거

--- PowSeat/main/utils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <algorithm>
-
-/** Clamp v into [lo, hi]. Works for any comparable type T. */
-template<typename T>
-static inline T clamp(T v, T lo, T hi){
-  return std::max(lo, std::min(v, hi));
-}
